6fa9aac661a779e1017ad8dac3635d0eae369b84
Erenalp <erenalp@alpbilisim.com.tr>
2025-05-25 17:57:26 +0300

    Business layer content created

---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void TInsert(T entity);
+        void TUpdate(T entity);
+        void TDelete(T entity); //Belirtilen id'ye sahip öğeyi silen bir metodu tanımlar
+        List<T> TGetAll(); //Tüm öğeleri içeren bir listeyi döndüren metodu tanımlar.
+        T TGetById(int id); //Belirtilen id'ye sahip öğeyi döndüren metodu tanımlar.
+    }
+}
--- CSharpEgitimKampi301/CSharpEgitimKampi301.PresentationLayer/frmCategory.cs
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.PresentationLayer/frmCategory.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CSharpEgitimKampi301.PresentationLayer
 {
-    public partial class Form1 : Form
+    public partial class frmCategory : Form
     {
-        public Form1()
+        public frmCategory()
         {
             InitializeComponent();
         }
